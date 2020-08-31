bf00379e35775644e457176b0465f1b4dfeb02ba
mohsinbohra <mohsinbohra20@gmail.com>
2020-08-31 16:15:20 +0530

    Editing the README to try out git add/commit

---
+++ src/Component/TicTacToe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TicTacToe.css';
+import Game from './Game';
+
+const TicTacToe = () => {
+    return (
+        <div>
+            <Game />
+        </div>
+    );
+};
+
+export default TicTacToe;
